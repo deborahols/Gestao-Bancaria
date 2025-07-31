56d41913e9945d6d7c933cd282f4ac487ad82ae8
Deborah <deborah.olisilva@gmail.com>
2025-07-30 21:47:36 -0400

    Ajustando indentação dos testes de movimentação

--- cypress/e2e/movimentacao/moviment.cy.js
+++ cypress/e2e/movimentacao/moviment.cy.js
@@ -9,59 +9,59 @@ describe('Cadastro de movimentação', () => {
         cy.cadConta(gerarConta())
     })
 
-    it('1- Movimentação com campos em branco', () => {
+        it('1- Movimentação com campos em branco', () => {
 
-        cy.get('[href="/movimentacao"]',).click()
+            cy.get('[href="/movimentacao"]',).click()
 
-        cy.contains('Cadastro de Movimentação').should('be.visible')
+            cy.contains('Cadastro de Movimentação').should('be.visible')
 
-        cy.get('button[type="submit"]').click()
+            cy.get('button[type="submit"]').click()
 
-        cy.contains('A pessoa é obrigatória').should('be.visible')
-        cy.contains('A Conta é obrigatória').should('be.visible')
-        cy.contains('Tipo Movimentação é obrigatório').should('be.visible')
-        cy.contains('Valor é obrigatório').should('be.visible')
-    })
+            cy.contains('A pessoa é obrigatória').should('be.visible')
+            cy.contains('A Conta é obrigatória').should('be.visible')
+            cy.contains('Tipo Movimentação é obrigatório').should('be.visible')
+            cy.contains('Valor é obrigatório').should('be.visible')
+        })
 
-    it('2- Deposito cadastrado com sucesso', () => {
+        it('2- Deposito cadastrado com sucesso', () => {
 
-        cy.get('[href="/movimentacao"]',).click()
+            cy.get('[href="/movimentacao"]',).click()
 
-        cy.contains('Cadastro de Movimentação').should('be.visible')
+            cy.contains('Cadastro de Movimentação').should('be.visible')
 
-        cy.get('#pessoaField').click()
-        cy.get('ul li').eq(1).click()
-        cy.get('#numeroContaField').click()
-        cy.get('ul li').eq(1).click()
-        cy.get('#tipoField').click()
-        cy.get('ul li').eq(1).click()
-        cy.get('#valorField').type(faker.string.numeric(6))
-        cy.get('button[type="submit"]').click()
+            cy.get('#pessoaField').click()
+            cy.get('ul li').eq(1).click()
+            cy.get('#numeroContaField').click()
+            cy.get('ul li').eq(1).click()
+            cy.get('#tipoField').click()
+            cy.get('ul li').eq(1).click()
+            cy.get('#valorField').type(faker.string.numeric(6))
+            cy.get('button[type="submit"]').click()
 
-        cy.contains('Movimentacão criada com sucesso!').should('be.visible')
-    })
+            cy.contains('Movimentacão criada com sucesso!').should('be.visible')
+        })
 
-    it('3- Saque realizado com sucesso', () => {
+        it('3- Saque realizado com sucesso', () => {
 
-        cy.get('[href="/movimentacao"]',).click()
+            cy.get('[href="/movimentacao"]',).click()
 
-        cy.contains('Cadastro de Movimentação').should('be.visible')
+            cy.contains('Cadastro de Movimentação').should('be.visible')
 
-        cy.get('#pessoaField').click()
-        cy.get('ul li').eq(1).click()
-        cy.get('#numeroContaField').click()
-        cy.get('ul li').eq(1).click()
-        cy.get('#tipoField').click()
-        cy.get('ul li').eq(2).click()
-        cy.get('#valorField').type(faker.string.numeric(5))
-        cy.get('button[type="submit"]').click()
+            cy.get('#pessoaField').click()
+            cy.get('ul li').eq(1).click()
+            cy.get('#numeroContaField').click()
+            cy.get('ul li').eq(1).click()
+            cy.get('#tipoField').click()
+            cy.get('ul li').eq(2).click()
+            cy.get('#valorField').type(faker.string.numeric(5))
+            cy.get('button[type="submit"]').click()
 
-        cy.contains('Movimentacão criada com sucesso!').should('be.visible')
-    })
+            cy.contains('Movimentacão criada com sucesso!').should('be.visible')
+        })
 
-    it('4 - Consulta de Extrato', () => {
+        it('4 - Consulta de Extrato', () => {
 
 
-    })
+        })
 
 })
