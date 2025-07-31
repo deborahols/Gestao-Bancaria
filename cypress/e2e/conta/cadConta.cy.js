d0c8e8956db0b4c7652ce626bfaeefc79d55b5cc
Deborah <deborah.olisilva@gmail.com>
2025-07-30 21:52:07 -0400

    Incluindo teste de exclusão de conta com movimentação vinculada

--- cypress/e2e/conta/cadConta.cy.js
+++ cypress/e2e/conta/cadConta.cy.js
@@ -8,55 +8,55 @@ describe ('Cadatro de Conta Bancaria', () => {
         cy.cadPessoa(gerarPessoa())
     })
 
-     it('1- Campos em branco', () => {
+        it('1- Campos em branco', () => {
 
-    cy.get('[href="/conta"]',).click()
-    cy.contains('Cadastro de Conta').should('be.visible')
-    cy.get('button[type="submit"]').click()
-    cy.contains('A pessoa é obrigatória').should('be.visible')
-    cy.contains('O número da conta é obrigatório').should('be.visible')
-    })
+            cy.get('[href="/conta"]',).click()
+            cy.contains('Cadastro de Conta').should('be.visible')
+            cy.get('button[type="submit"]').click()
+            cy.contains('A pessoa é obrigatória').should('be.visible')
+            cy.contains('O número da conta é obrigatório').should('be.visible')
+        })
 
-    it('2- Conta já existente', () => {
+        it('2- Conta já existente', () => {
 
-    const contaDupli = faker.string.numeric(15);
+            const contaDupli = faker.string.numeric(15);
 
-    cy.get('[href="/conta"]').click()
-    cy.contains('Cadastro de Conta').should('be.visible')
+            cy.get('[href="/conta"]').click()
+            cy.contains('Cadastro de Conta').should('be.visible')
+            
+            // abre o dropdown
+            cy.get('#pessoaField').click()
+
+            // seleciona a lista de opções, clica na opção pelo indice
+            cy.get('ul li').eq(0).click()
+            cy.get('#numeroContaField').type(contaDupli)
+            cy.get('button[type="submit"]').click()
+
+            cy.contains('Conta criada com sucesso!').should('be.visible')
+
+            cy.get('#pessoaField').click()
+            cy.get('ul li').eq(0).click()
+            cy.get('#numeroContaField').type(contaDupli)
+            cy.get('button[type="submit"]').click()
+
+            cy.contains('Conta já cadastrada').should('be.visible')
+        })
+        
+        it('3- Conta criada com sucesso', () => {
     
-    // abre o dropdown
-    cy.get('#pessoaField').click()
+            cy.get('button[type="submit"]').click()
+            cy.get('[href="/conta"]').click()
+            cy.contains('Cadastro de Conta').should('be.visible')
+            
+            // abre o dropdown
+            cy.get('#pessoaField').click()
 
-    // seleciona a lista de opções, clica na opção pelo indice
-    cy.get('ul li').eq(0).click()
-    cy.get('#numeroContaField').type(contaDupli)
-    cy.get('button[type="submit"]').click()
+            // seleciona a lista de opções, clica na opção pelo indice
+            cy.get('ul li').eq(0).click()
+            cy.get('#numeroContaField').type(faker.string.numeric(15))
+            cy.get('button[type="submit"]').click()
 
-    cy.contains('Conta criada com sucesso!').should('be.visible')
-
-    cy.get('#pessoaField').click()
-    cy.get('ul li').eq(0).click()
-    cy.get('#numeroContaField').type(contaDupli)
-    cy.get('button[type="submit"]').click()
-
-    cy.contains('Conta já cadastrada').should('be.visible')
-    })
-    
-    it('3- Conta criada com sucesso', () => {
-   
-    cy.get('button[type="submit"]').click()
-    cy.get('[href="/conta"]').click()
-    cy.contains('Cadastro de Conta').should('be.visible')
-    
-    // abre o dropdown
-    cy.get('#pessoaField').click()
-
-    // seleciona a lista de opções, clica na opção pelo indice
-    cy.get('ul li').eq(0).click()
-    cy.get('#numeroContaField').type(faker.string.numeric(15))
-    cy.get('button[type="submit"]').click()
-
-    cy.contains('Conta criada com sucesso!').should('be.visible')
-    })
+            cy.contains('Conta criada com sucesso!').should('be.visible')
+        })
 
 })
--- cypress/e2e/conta/deletConta.cy.js
+++ cypress/e2e/conta/deletConta.cy.js
@@ -9,45 +9,50 @@ describe ('Exclusão de conta', () => {
         cy.cadConta(gerarConta())
     })
 
-   it('1- Cancelamento de exclusão de Conta', () => {
+        it('1- Cancelamento de exclusão de Conta', () => {
 
-    const conta = {
-        numeroContaField: faker.string.numeric(15)
-    }
+            const conta = {
+                numeroContaField: faker.string.numeric(15)
+            }
 
-    cy.cadConta(conta)
+            cy.cadConta(conta)
 
-    cy.contains(conta.numeroContaField)
-      .parent() // pega a linha onde o nome está
-      .find('.text-red-600')
-      .click()
-      
-    cy.contains('Cancelar').click()
+            cy.contains(conta.numeroContaField)
+            .parent() // pega a linha onde o nome está
+            .find('.text-red-600')
+            .click()
+            
+            cy.contains('Cancelar').click()
 
-    cy.contains(conta.numeroContaField).should('be.visible')
-})
+            cy.contains(conta.numeroContaField).should('be.visible')
+        })
 
-    /*it ('2 - Conta não excluída devido a existencia de movimentação', () => {
+        it ('2 - Conta não excluída devido a existencia de movimentação', () => {
 
-})*/
+            cy.get('.text-red-600').first().click()
+            cy.contains('Excluir').click()
+            
+            cy.contains('Não é possível excluir uma conta com movimentação vinculada').should('be.visible')
+        
+        })
 
-    it ('3 - Conta excluída com sucesso', () => {
-    
-    const conta = {
-        numeroContaField: faker.string.numeric(15)
-    }
+        it ('3 - Conta excluída com sucesso', () => {
+            
+            const conta = {
+                numeroContaField: faker.string.numeric(15)
+            }
 
-    cy.cadConta(conta)
+            cy.cadConta(conta)
 
-    cy.contains(conta.numeroContaField)
-      .parent() // pega a linha onde o nome está
-      .find('.text-red-600')
-      .click()
+            cy.contains(conta.numeroContaField)
+            .parent() // pega a linha onde o nome está
+            .find('.text-red-600')
+            .click()
 
-    cy.contains('Excluir').click()
+            cy.contains('Excluir').click()
 
-    cy.contains('Conta excluída com sucesso').should('be.visible')
-    cy.contains(conta.numeroContaField).should('not.exist')
-})
+            cy.contains('Conta excluída com sucesso').should('be.visible')
+            cy.contains(conta.numeroContaField).should('not.exist')
+        })
 
 })
