faefccd26ecab2de9956b935a7b61dc9a5c4413a
Deborah <deborah.olisilva@gmail.com>
2025-07-30 21:53:54 -0400

    Ajustando indentação do cadastro de conta

--- cypress/e2e/conta/editConta.cy.js
+++ cypress/e2e/conta/editConta.cy.js
@@ -9,46 +9,46 @@ describe ('Edição de Conta', () => {
         cy.cadConta(gerarConta())
     })
 
-    it('1 - Validar edição com campo Pessoa em branco', () => {
+        it('1 - Validar edição com campo Pessoa em branco', () => {
 
-    cy.get('.text-blue-600').first().click()
-    cy.get('#pessoaField').clear().click()
-    cy.get('button[type="submit"]').click()
-    cy.contains('Bad Request').should('be.visible')
-    })
+            cy.get('.text-blue-600').first().click()
+            cy.get('#pessoaField').clear().click()
+            cy.get('button[type="submit"]').click()
+            cy.contains('Bad Request').should('be.visible')
+        })
 
-     it('2 - Validar edição com campo Número da Conta em branco', () => {
+        it('2 - Validar edição com campo Número da Conta em branco', () => {
 
-    cy.get('.text-blue-600').first().click()
-    cy.get('#numeroContaField').clear()
-    cy.get('button[type="submit"]').click()
-    cy.contains('O número da conta é obrigatório').should('be.visible')
-    })
+            cy.get('.text-blue-600').first().click()
+            cy.get('#numeroContaField').clear()
+            cy.get('button[type="submit"]').click()
+            cy.contains('O número da conta é obrigatório').should('be.visible')
+        })
 
-    it('3 - Validar edição do nome da pessoa', () => {
+        it('3 - Validar edição do nome da pessoa', () => {
 
-    cy.get('.text-blue-600').first().click()
-    cy.get('#pessoaField').click()
-    cy.get('ul li').eq(1).click()
-    cy.get('button[type="submit"]').click()
-    cy.contains('Conta atualizada com sucesso!').should('be.visible')
-    })
+            cy.get('.text-blue-600').first().click()
+            cy.get('#pessoaField').click()
+            cy.get('ul li').eq(1).click()
+            cy.get('button[type="submit"]').click()
+            cy.contains('Conta atualizada com sucesso!').should('be.visible')
+        })
 
-     it('4 - Validar edição do número da conta', () => {
+        it('4 - Validar edição do número da conta', () => {
 
-    cy.get('.text-blue-600').first().click()
-    cy.get('#numeroContaField').clear().type(faker.string.numeric(15))
-    cy.get('button[type="submit"]').click()
-    cy.contains('Conta atualizada com sucesso!').should('be.visible')
-    })
+            cy.get('.text-blue-600').first().click()
+            cy.get('#numeroContaField').clear().type(faker.string.numeric(15))
+            cy.get('button[type="submit"]').click()
+            cy.contains('Conta atualizada com sucesso!').should('be.visible')
+        })
 
-    it.only('5 - Validar edição de todos os campos', () => {
+        it.only('5 - Validar edição de todos os campos', () => {
 
-    cy.get('.text-blue-600').first().click()
-    cy.get('#pessoaField').click()
-    cy.get('ul li').eq(1).click()
-    cy.get('#numeroContaField').clear().type(faker.string.numeric(15))
-    cy.get('button[type="submit"]').click()
-    cy.contains('Conta atualizada com sucesso!').should('be.visible')
-    })
+            cy.get('.text-blue-600').first().click()
+            cy.get('#pessoaField').click()
+            cy.get('ul li').eq(1).click()
+            cy.get('#numeroContaField').clear().type(faker.string.numeric(15))
+            cy.get('button[type="submit"]').click()
+            cy.contains('Conta atualizada com sucesso!').should('be.visible')
+        })
 })
